cf59dd6eb5056bb0b4c9c95ccd49e93c8c328143
Ryo Okazaki <okazaki3905@gmail.com>
2025-12-14 18:01:31 +0900

    feat: start by debug mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 
 cd /workdir/src && npm install
 
-cd /workdir/src && npm run dev
+if [ "$NEXT_SERVER_DEBUG" = "1" ]; then
+  cd /workdir/src && npm run dev:debug
+else
+  cd /workdir/src && npm run dev
+fi
