ce13fdec142f95d92369e8b5302933f489ebb5c7
Ryo Okazaki <okazaki3905@gmail.com>
2025-12-30 11:26:13 +0900

    fix: asset-prefixのconfigを設定

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || "",
 };
 
 export default nextConfig;
